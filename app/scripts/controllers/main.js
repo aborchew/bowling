bf09d5e6eb3d6f232f08ac43e7d0b775a7297e0a
aborche <ari.borchew@searshc.com>
2014-01-27 15:43:17 -0600

    UI Complete for single-player scoring

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,4 +5,14 @@ angular.module('bowlingApp')
 
     $scope.game = new Game();
 
+    $scope.checkPins = function(pins) {
+      if($scope.game.frames[$scope.game.frames.length-1].length == 1) {
+        if($scope.game.frames[$scope.game.frames.length-1][0] + pins <= 10) {
+          return false;
+        } else if($scope.game.frames[$scope.game.frames.length-1][0] !== 10) {
+          return true;
+        }
+      }
+    }
+
   }]);
--- app/scripts/game.js
+++ app/scripts/game.js
@@ -8,19 +8,31 @@ function Game () {
 
   this.frames = [[]];
 
+  this.new = function () {
+    this.frames = [[]];
+  }
+
   this.bowl = function (pins) {
 
     if(this.frames.length <= 10) {
       var frames = this.frames
       , framesLength = frames.length
-      , currentFrame = frames[framesLength-1]
+      , currentFrame = frames[framesLength-1],
+      frameTally = simpleReduce(currentFrame);
       ;
 
-      if((currentFrame.length === 2 || simpleReduce(currentFrame) === 10) && framesLength < 10) {
-        frames[framesLength] = [];
-        this.bowl(pins);
-      } else {
+      if(framesLength < 10 && frameTally + pins <= 10 && currentFrame.length < 2) {
         currentFrame[currentFrame.length] = pins;
+      } else if(framesLength < 10 && (frameTally === 10 || currentFrame.length === 2)) {
+        frames[framesLength] = [pins]
+      } else if(framesLength === 10 && currentFrame.length < 3) {
+        if(frameTally === 10 && currentFrame.length < 2) {
+          currentFrame[currentFrame.length] = pins;
+        } else if(frameTally >= 10 && currentFrame.length === 2) {
+          currentFrame[currentFrame.length] = pins;
+        } else if(frameTally < 10 && currentFrame.length == 1 && frameTally + pins <= 10) {
+          currentFrame[currentFrame.length] = pins;
+        }
       }
     }
 
@@ -49,7 +61,7 @@ function Game () {
       if(frameTally >= 10) {
         if(frameLength == 2 && frames[i+1]) {
           tally += frames[i+1][0] || 0;
-        } else if(frameLength == 1) {
+        } else if(frameLength == 1 && frames[i+1]) {
           tally += frames[i+1][0] || 0;
           if(frames[i+1][0] == 10 && frames[i+2]) {
             tally += frames[i+2][0] || 0;
@@ -68,41 +80,33 @@ function Game () {
   this.displayFrame = function (frameIndex) {
 
     var frame = this.frames[frameIndex];
-    var constructedFrame = [];
 
-    if(frame && frameIndex !== 9) {
-      if(frame.length === 1 && frame[0] === 10) {
-        return ['','X'];
-      } else if(frame.length === 2 && frame[0] + frame[1] === 10) {
-        return [frame[0],'/'];
-      } else {
-        return frame;
-      }
-    } else if(frame && frameIndex === 9) {
-
-      if(frame[0] === 10) {
-        constructedFrame[0] = 'X';
-        if(frame[1] === 10) {
-          constructedFrame[1] = 'X';
-        }
-      } else if(frame[0] + frame[1] === 10) {
-        constructedFrame[0] = frame[0];
-        constructedFrame[1] = '/';
-      }
-
-      if(frame[2]) {
-        if(frame[2] === 10) {
-          constructedFrame[2] ='X';
+    if(frameIndex !== 9 && frame) {
+      for(var i = 0; i < frame.length; i++) {
+        if(i === 0 && frame[i] === 10) {
+          return ['','X'];
+        } else if(frame.length == 2 && simpleReduce(frame) === 10) {
+          return [frame[0],'/'];
         } else {
-          constructedFrame[2] = frame[2];
+          return frame;
         }
       }
-
-      return constructedFrame;
-
+    } else if(frameIndex === 9 && frame) {
+      var constructed = [];
+      for(var j = 0; j < frame.length; j++) {
+        if(frame[j] === 10) {
+          constructed[j] = 'X';
+        } else {
+          constructed[j] = frame[j];
+        }
+        if(frame.length > 1 && simpleReduce([frame[0],frame[1]]) === 10 && frame[0] !== 10) {
+          constructed[1] = '/';
+        }
+      }
+      return constructed;
     }
 
-    return [];
+    return;
 
   }
 
